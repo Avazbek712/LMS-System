d68de87403957f6693bda24e54c4cd089627e0c6
Avazbek712 <avazbekloz123@gmail.com>
2025-07-20 20:35:10 +0500

    first commit

---
+++ src/test/java/uz/imv/lmssystem/LmsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.imv.lmssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
