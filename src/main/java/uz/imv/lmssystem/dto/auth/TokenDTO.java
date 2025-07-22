9424c03fd800c6d38a7ce180261fa28769c5cab0
Avazbek712 <avazbekloz123@gmail.com>
2025-07-22 14:30:03 +0500

    minor

---
+++ src/main/java/uz/imv/lmssystem/dto/auth/TokenDTO.java
@@ -0,0 +1,18 @@
+package uz.imv.lmssystem.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by Avazbek on 18/07/25 12:20
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDTO {
+
+    private String token;
+
+    private long expires;
+}
