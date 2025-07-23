e4baab9016bdcace4f38d651ef9fff9c6771a758
Avazbek712 <avazbekloz123@gmail.com>
2025-07-23 17:50:28 +0500

    minor

---
+++ src/main/java/uz/imv/lmssystem/dto/NewEmployeeResponse.java
@@ -0,0 +1,19 @@
+package uz.imv.lmssystem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by Avazbek on 23/07/25 12:35
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NewEmployeeResponse {
+
+    private String username;
+
+    private String password;
+
+}
