c120f066d92e2e5a3c095fb041c6831f859a5d40
Avazbek712 <avazbekloz123@gmail.com>
2025-07-24 11:08:27 +0500

    course CRUD

---
+++ src/main/java/uz/imv/lmssystem/controller/CourseController.java
@@ -0,0 +1,61 @@
+package uz.imv.lmssystem.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import uz.imv.lmssystem.dto.CourseDTO;
+import uz.imv.lmssystem.dto.response.CourseResponseDTO;
+import uz.imv.lmssystem.enums.PermissionsEnum;
+import uz.imv.lmssystem.service.CourseService;
+
+import java.util.List;
+
+/**
+ * Created by Avazbek on 24/07/25 10:58
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/courses")
+public class CourseController {
+
+    private final CourseService courseService;
+
+    @GetMapping
+    @PreAuthorize("hasAnyAuthority('COURSE_READ')")
+    public ResponseEntity<List<CourseDTO>> getAllCourses() {
+
+        return ResponseEntity.ok(courseService.getAll());
+    }
+
+    @GetMapping("{id}")
+    @PreAuthorize("hasAnyAuthority('COURSE_READ')")
+    public ResponseEntity<CourseDTO> getCourseById(@PathVariable Long id) {
+
+        return ResponseEntity.ok(courseService.getById(id));
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAnyAuthority('COURSE_CREATE')")
+    public ResponseEntity<CourseResponseDTO> createCourse(@Valid @RequestBody
+                                                          CourseDTO courseDTO) {
+        return ResponseEntity.ok(courseService.save(courseDTO));
+    }
+
+    @PutMapping("{id}")
+    @PreAuthorize("hasAnyAuthority('COURSE_UPDATE')")
+    public ResponseEntity<CourseResponseDTO> updateCourse(@PathVariable Long id,
+                                                          @Valid @RequestBody CourseDTO courseDTO) {
+        return ResponseEntity.ok(courseService.update(id, courseDTO));
+    }
+
+    @DeleteMapping("{id}")
+    @PreAuthorize("hasAnyAuthority('COURSE_DELETE')")
+    public ResponseEntity<Void> deleteCourse(@PathVariable Long id) {
+        courseService.deleteById(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+}
---
+++ src/main/java/uz/imv/lmssystem/repository/CourseRepository.java
@@ -0,0 +1,13 @@
+package uz.imv.lmssystem.repository;
+
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import uz.imv.lmssystem.entity.Course;
+
+import java.util.Optional;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+    Optional<Course> getByName(@NotBlank(message = "name cannot be blank") String name);
+}
