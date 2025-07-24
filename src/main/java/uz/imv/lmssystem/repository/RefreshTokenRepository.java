21c8ce2380b8d98339ce8c78a3af0856dbb38411
Avazbek712 <avazbekloz123@gmail.com>
2025-07-24 11:09:08 +0500

    Deleted

--- src/main/java/uz/imv/lmssystem/repository/RefreshTokenRepository.java
+++ src/main/java/uz/imv/lmssystem/repository/RefreshTokenRepository.java
@@ -2,8 +2,6 @@ package uz.imv.lmssystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RestController;
-import uz.imv.lmssystem.entity.RefreshToken;
 import uz.imv.lmssystem.entity.User;
 
 import java.util.Optional;
