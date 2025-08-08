769f10b4b12374a52e854332916168e979780679
Acer NitroV16 <abdulaziznurmatullayev@gmail.com>
2025-08-08 09:12:30 +0500

    addition of new methods

--- src/main/java/uz/imv/lmssystem/repository/UserRepository.java
+++ src/main/java/uz/imv/lmssystem/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package uz.imv.lmssystem.repository;
 
 import jakarta.validation.constraints.NotBlank;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
@@ -16,4 +19,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(@NotBlank(message = "username must not be blank") String username);
 
     Optional<User> findByName(String name);
+
+    Page<User> findAll(Specification<User> specification, Pageable pageable);
 }
