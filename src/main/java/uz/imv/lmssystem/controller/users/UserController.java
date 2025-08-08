17afb963609850624ed3833e09b0d9933114abec
Acer NitroV16 <abdulaziznurmatullayev@gmail.com>
2025-08-08 09:21:45 +0500

    addition of new methods

--- src/main/java/uz/imv/lmssystem/controller/users/UserController.java
+++ src/main/java/uz/imv/lmssystem/controller/users/UserController.java
@@ -7,17 +7,14 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import uz.imv.lmssystem.dto.auth.UpdatePasswordDTO;
 import uz.imv.lmssystem.dto.UserDTO;
 import uz.imv.lmssystem.dto.UserUpdateDTO;
-import uz.imv.lmssystem.dto.filter.GroupFilterDTO;
+import uz.imv.lmssystem.dto.auth.UpdatePasswordDTO;
 import uz.imv.lmssystem.dto.filter.UserFilterDTO;
+import uz.imv.lmssystem.dto.request.ChangedRoleRequest;
 import uz.imv.lmssystem.dto.response.PageableDTO;
-import uz.imv.lmssystem.dto.response.RespUserDTO;
 import uz.imv.lmssystem.entity.User;
 import uz.imv.lmssystem.service.users.UserService;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -36,9 +33,9 @@ public class UserController {
 
     @PatchMapping("change-role/{id}")
     @PreAuthorize("hasAuthority('ROLE_MANAGE')")
-    public ResponseEntity<?> changeRole(@PathVariable Long id, @Valid @RequestBody Long roleId) {
+    public ResponseEntity<?> changeRole(@PathVariable Long id, @Valid @RequestBody ChangedRoleRequest dto) {
 
-        return ResponseEntity.ok(userService.changeRole(id, roleId));
+        return ResponseEntity.ok(userService.changeRole(id, dto));
     }
 
     @PutMapping("update-user")
@@ -78,5 +75,4 @@ public class UserController {
                                     @RequestParam(defaultValue = "10") int size) {
         return userService.getFilteredEmployees(filter, page, size);
     }
-
 }
