aa6a3c882bfc4871a07275d566759e278415a93a
Acer NitroV16 <abdulaziznurmatullayev@gmail.com>
2025-08-08 09:12:34 +0500

    addition of new methods

--- src/main/java/uz/imv/lmssystem/mapper/UserMapper.java
+++ src/main/java/uz/imv/lmssystem/mapper/UserMapper.java
@@ -3,11 +3,15 @@ package uz.imv.lmssystem.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
 import uz.imv.lmssystem.dto.UserDTO;
+import uz.imv.lmssystem.dto.response.RespUserDTO;
 import uz.imv.lmssystem.entity.Role;
 import uz.imv.lmssystem.entity.User;
 
-@Mapper(componentModel = "spring")
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
 
     @Mapping(source = "role", target = "role", qualifiedByName = "roleToString")
@@ -17,5 +21,13 @@ public interface UserMapper {
     static String roleToString(Role role) {
         return role != null ? role.getName() : null;
     }
+
+    @Mapping(target = "fullName", expression = "java(user.getName() + \" \" + user.getSurname())")
+    @Mapping(target = "roleName", source = "role.name")
+    @Mapping(target = "deleted", source = "deleted")
+    RespUserDTO toRespDTO(User user);
+
+    List<RespUserDTO> toRespDTO(List<User> users);
+
 }
 
