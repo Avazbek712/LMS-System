3abaf43335236195d1096570b1ef8dabbd3cf6d8
Acer NitroV16 <abdulaziznurmatullayev@gmail.com>
2025-08-08 09:13:02 +0500

    addition of new methods

--- src/main/java/uz/imv/lmssystem/repository/ExpenseRepository.java
+++ src/main/java/uz/imv/lmssystem/repository/ExpenseRepository.java
@@ -1,5 +1,8 @@
 package uz.imv.lmssystem.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import uz.imv.lmssystem.entity.Expense;
@@ -10,4 +13,5 @@ public interface ExpenseRepository extends JpaRepository<Expense, Long> {
     @Query("SELECT COALESCE(SUM(e.amount), 0) FROM Expense e")
     BigDecimal getTotalExpense();
 
+    Page<Expense> findAll(Specification<Expense> spec, Pageable pageable);
 }
